414e93a185a18ec99775e6f2a11600760dbc36d1
EmilSchroder <eschro66@gmail.com>
2018-09-06 21:48:49 +1200

    room #3 updated

--- seeds/rooms.js
+++ seeds/rooms.js
@@ -17,6 +17,14 @@ exports.seed = (knex, Promise)=> {
         description: "Enjoy a wonderful night in our highly specialised EDA Tech Suite. Our wide array of couches have room for your entire team to settle in for your all-night coding session. Are your team members likely to take short naps while their code compiles? Curl up on a spare couch! Does your team tend to charge through together and crash simultaneously at the end? A large supply of thick blankets is available to wrap up in so you all can slump over in our cushy armchairs when you have finally deployed. The carpeted floor also comes highly recommended as a substitute when your tallest teammate takes up all the couch space. A full kitchen is also available. (Note: caffeine supply is limited. We know you, developers. We can't afford your energy drinks. Bring your own.)",
         capacity: 20,
         img: "https://devacademy.co.nz/wp-content/uploads/2017/06/Dev-Academy-Space.jpg"
+      },
+      {
+        id: 3,
+        room_name: "Die Schmetterlinghaus",
+        available: true,
+        description: "Some rude guests have suggested that there is such a thing as too many butterflies. If you feel this way then leave IMMEDIATELY! Otherwise if you are amoung the cultured few then please enjoy everything Die Schmetterlinghaus has to offer. Drift off to sleep to the sound of incesent flapping! Wake up to find a thorax in your mouth! Desperately try not to think of thousands of creepy bug eyes watching your every move! You don't need to get hopelessly lost in a rainforest to discover these joys, book today for an experience you probably will not regret. UPDATE: The next 1 guests to book will receive a complimentary head-to-toe coating in necter by hotel staff.",
+        capacity: 3,
+        img: "http://www.schmetterlinghaus.at/fileadmin/_processed_/csm_Atlas_Panorama__Schmetterlinghaus_41693ed3fb.jpg"
       }
     ])
   })
