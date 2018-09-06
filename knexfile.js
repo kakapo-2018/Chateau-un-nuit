f1ac52967a852aede529b7dd83468fc8aa718dd7
nepcam <cameron.nepe@gmail.com>
2018-09-07 09:25:56 +1200

    connection to pg should be string not object

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,7 @@ module.exports = {
 
     client: 'postgresql',
     connection: 'postgres://ifsolquqdfymwl:1feca99589feb4bd8769c858897312fec80ee5c450d02379d2954a9bfcd2229a@ec2-54-221-237-246.compute-1.amazonaws.com:5432/ddam9gk3ikmjvq',
+
     migrations: {
       tableName: 'knex_migrations'
     }
