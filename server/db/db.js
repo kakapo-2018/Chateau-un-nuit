908835919ee6a3eb18363ce98a051b7bf85ec391
nepcam <cameron.nepe@gmail.com>
2018-09-07 15:23:10 +1200

    configurte the env for heroku

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,5 @@
-const config = require('../../knexfile').development
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../../knexfile')[environment]
 const db = require('knex')(config)
 
 function getAllRooms(testConn){
