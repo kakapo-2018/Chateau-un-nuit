d64f65b30f79d97e0b486d63cff6e74652205690
Nepcam <cameron.nepe@gmail.com>
2018-09-19 17:35:58 +1200

    Steps 10 to 14

--- server/db/users.js
+++ server/db/users.js
@@ -2,18 +2,29 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('../../knexfile')[environment]
 const connection = require('knex')(config)
 
+//Get connection for the database
+const connection = require('./connection')
 //Get password details for user from hash file
 const hash = require('../auth/hash');
 
 module.exports = {
   createUser,
-  userExists
+  userExists,
+  getUserByName
 };
 
 function createUser(username, password, conn) {
   const db = conn || connection;
   const passwordHash = hash.generate(password);
   return db('users').insert({ username, hash: passwordHash });
+}
+
+function getUserByName (username, conn) {
+  const db = conn || connection
+  return db('users')
+  .select()
+  .where('username', username)
+  .first()
 }
 
 function userExists(username, conn) {
--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const server = require('./server');
 
+//Calling config function to enable dotenv package. This allows environment variables to be available
+require('dotenv').config()
+
 const port = process.env.PORT || 3000;
 
 server.listen(port, function() {
