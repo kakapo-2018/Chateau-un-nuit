28178c59436183e64f2cbcb13956d06a0bf90b5f
Nepcam <cameron.nepe@gmail.com>
2018-09-19 18:32:07 +1200

    Steps 15 & 16

---
+++ server/auth/token.js
@@ -0,0 +1,30 @@
+
+//Brings in the jsonwebtoken
+const jwt = require('jsonwebtoken')
+
+//Picks up server/db/users.js file
+const db = require('../db/users')
+
+module.exports = {
+  issue
+}
+
+function issue (req, res) {
+  db.getUserByName(req.body.username)
+    .then(user => {
+      const token = createToken(user, process.env.JWT_SECRET)
+      res.json({
+        message: 'Authentication successful.',
+        token
+      })
+    })
+}
+
+function createToken (user, secret) {
+  return jwt.sign({
+    id: user.id,
+    username: user.username
+  }, secret, {
+     expiresIn: '1d'     
+  })
+} 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,8 +7,12 @@ const router = express.Router();
 //Get users database
 const { userExists, createUser } = require('../db/users');
 
+//Bring in token from token file
+const token = require('../auth/token')
+
+
 //Post register from route
-router.post('/register', register);
+router.post('/register', register, token.issue);
 
 //Pick up password from user
 function register(req, res) {
@@ -16,16 +20,16 @@ function register(req, res) {
 }
 
 //See if User exists and if not then create User
-function register(req, res) {
+function register(req, res, next) {
   userExists(req.body.username)
     .then(exists => {
       if (exists) {
         return res.status(400).send({ message: 'User exists' });
       }
-      createUser(req.body.username, req.body.password).then(() =>
-        res.status(201).end()
-      );
-    })
+      createUser(req.body.username, req.body.password)
+      .then(() => next())
+        //res.status(201).end()
+    });
     .catch(err => {
       res.status(500).send({ message: err.message });
     });
